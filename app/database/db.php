7947ac4bc1455034454c48f435504bd53b727a22
B4N4N41C <mochalin.nikolay2017@yandex.ru>
2023-10-20 22:25:18 +0300

    End first day

--- app/database/db.php
+++ app/database/db.php
@@ -8,23 +8,75 @@ function tt($value)
     print_r($value);
     echo '</pre>';
 }
-
-function selectAll($table)
-{
-    global $pdo;
-
-    $sql = "SELECT * FROM $table";
-
-    $query = $pdo->prepare($sql);
-    $query->execute();
+//Проверка выполнения запроса к БД
+function dbCheckError($query){
     $errInfo = $query->errorInfo();
 
     if ($errInfo[0] !== PDO::ERR_NONE) {
         echo $errInfo[2];
         exit();
     }
+    return true;
+} 
 
+//Запрос на получение данных одной с таблицы
+function selectAll($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+    
+    if (!empty($params)){
+        $i = 0;
+        foreach($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'" . $value . "'";
+            }
+            if($i === 0){
+                $sql = $sql . " WHERE $key=$value";
+            }else{
+                $sql = $sql . " AND $key=$value";
+            }
+            $i++;
+        }
+    }
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
     return $query->fetchAll();
 }
+//Запрос на получение данных одной строки с выбранной таблицы
+function selectOne($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+    
+    if (!empty($params)){
+        $i = 0;
+        foreach($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'" . $value . "'";
+            }
+            if($i === 0){
+                $sql = $sql . " WHERE $key=$value";
+            }else{
+                $sql = $sql . " AND $key=$value";
+            }
+            $i++;
+        }
+    }
+//    $sql = $sql . " LIMIT 1";
 
-tt(selectAll('users'));
+
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+    return $query->fetch();
+}
+
+$params = [
+    'admin' => 1,
+    'username' => 'Nikolay'
+];
+
+//tt(selectAll('users', $params));
+tt(selectOne('users'));
